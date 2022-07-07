58ce0678b4fe54e123dc530a9df8a51de1078362
Evgen-dev1989 <camkaenota@gmail.com>
2022-07-07 10:50:46 +0200

    1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ZohoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('zohocrmauth', [ZohoController::class, 'auth'])->name('zohocrmauth');
+Route::get('zohocrm', [ZohoController::class, 'store'])->name('zohocrm');
